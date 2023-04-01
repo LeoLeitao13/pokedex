c3e867719adf133ef4665a7ecbdacaddce7b2366
Leonardo Leitao <102124645+LeoLeitao13@users.noreply.github.com>
2023-04-01 14:48:25 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonId = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_imagem");
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input_search");
+
+const prev = document.querySelector(".btn-prev");
+const next = document.querySelector(".btn-next");
+
+let searchPokemon = 1;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  );
+
+  if (APIResponse.status == 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+};
+
+const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = "Loading...";
+  pokemonId.innerHTML = "";
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = "block";
+    pokemonName.innerHTML = data.name;
+    pokemonId.innerHTML = data.id;
+    pokemonImage.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
+
+    input.value = "";
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = "none";
+    pokemonName.innerHTML = "Not Found :/ ";
+    pokemonId.innerHTML = "";
+  }
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+prev.addEventListener("click", () => {
+  if (searchPokemon > 1) {
+    searchPokemon--;
+    renderPokemon(searchPokemon);
+  }
+});
+
+next.addEventListener("click", () => {
+  searchPokemon++;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
